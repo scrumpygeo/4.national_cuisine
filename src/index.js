a6b04e42c6b2562e0c66149da5356b818a70c58f
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-02-10 11:05:38 +0000

    wrap App in Provider

--- src/index.js
+++ src/index.js
@@ -5,4 +5,11 @@ import { createStore } from "redux";
 import App from "./components/App";
 import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+//  wrap App in Provider and passed it prop 'store'
+//    created from our reducers
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
